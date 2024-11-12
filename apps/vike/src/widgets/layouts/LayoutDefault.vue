b4287e0a5e888d6a2de1b4a67a8119141e889103
7iomka <7iomka@gmail.com>
2024-11-13 00:56:29 +0200

    fix: [vike] add missing providers

--- apps/vike/src/widgets/layouts/LayoutDefault.vue
+++ apps/vike/src/widgets/layouts/LayoutDefault.vue
@@ -1,20 +1,23 @@
 <script lang="ts" setup>
   import { AppHeader } from '../app-header';
+  import Providers from '@/widgets/layouts/Providers.vue';
 </script>
 
 <template>
-  <div
-    data-id="layout-root"
-    class="flex min-h-[calc(100vh*1.125)] xs:min-h-screen flex-col
-      max-w-[1280px] mx-auto"
-  >
-    <AppHeader />
-    <div data-id="wrapper" class="flex-1 px-[--wrapper-px] py-[--wrapper-py]">
-      <main class="relative w-full min-w-0 bg-muted/25 p-5 min-h-full">
-        <slot />
-      </main>
+  <Providers>
+    <div
+      data-id="layout-root"
+      class="flex min-h-[calc(100vh*1.125)] xs:min-h-screen flex-col
+        max-w-[1280px] mx-auto"
+    >
+      <AppHeader />
+      <div data-id="wrapper" class="flex-1 px-[--wrapper-px] py-[--wrapper-py]">
+        <main class="relative w-full min-w-0 bg-muted/25 p-5 min-h-full">
+          <slot />
+        </main>
+      </div>
     </div>
-  </div>
+  </Providers>
 </template>
 
 <style>
